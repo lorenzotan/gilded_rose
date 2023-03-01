9df875e30400ef83f948daf706eb9d1300d8076d
Lorenzo Tan <lorenzo.tan@gmail.com>
2023-02-28 19:49:33 -0800

    breaking out Item class into its own file

---
+++ lib/item.py
@@ -0,0 +1,9 @@
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name    = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+
+    def __repr__(self) -> str:
+        return f"{self.name}, {self.sell_in}, {self.quality}"
