06b40efd5861a44750adba1ddc3c72db699d61a7
Lorenzo Tan <lorenzo.tan@gmail.com>
2023-03-01 03:47:07 -0800

    adding tests for ItemProxy class

---
+++ tests/test_item_proxy.py
@@ -0,0 +1,29 @@
+import pytest
+from ..lib import ItemProxy
+
+def test_increment_quality():
+    item = ItemProxy('Item', 5, 10)
+    item.increment_quality()
+
+    assert item.quality == 11
+
+
+def test_decrement_quality():
+    item = ItemProxy('Item', 5, 10)
+    item.decrement_quality()
+
+    assert item.quality == 9
+
+
+def test_reset_quality():
+    item = ItemProxy('Item', 5, 10)
+    item.reset_quality()
+
+    assert item.quality == 0
+
+
+def test_decrement_sell_in():
+    item = ItemProxy('Item', 5, 10)
+    item.decrement_sell_in()
+
+    assert item.sell_in == 4
